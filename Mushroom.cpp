04cbd152541014a4dc083e3a71870843c330f2ec
Azotronix <antoine.horion17@gmail.com>
2025-05-08 10:49:38 +0200

    les champignons ne disparaissent plus de la roche

--- Mushroom.cpp
+++ Mushroom.cpp
@@ -4,48 +4,11 @@
 Mushroom::Mushroom(int x, int y, int initialCooldown) : Bloc(x, y, MUSHROOM, sf::Color::Magenta) {
     initialCooldown_ = initialCooldown;
     spreadCooldown_ = initialCooldown_;
+    hooked_ = false;
 }
 void Mushroom::update(Map* map) {
-    if (--spreadCooldown_ <= 0) {
-        spreadCooldown_ = 30;
 
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dy = -1; dy <= 1; dy++) {
-                if (dx == 0 && dy == 0) continue;
-
-                int nx = getX() + dx;
-                int ny = getY() + dy;
-
-                if (map->inBounds(nx, ny)) {
-                    Bloc* adjacent = map->getBlock(nx, ny);
-
-                    if (adjacent == nullptr) {
-                        // Vérifie s'il y a des blocs STONE autour de la case vide
-                        int stoneCount = 0;
-                        for (int rx = -1; rx <= 1; rx++) {
-                            for (int ry = -1; ry <= 1; ry++) {
-                                if (rx == 0 && ry == 0) continue;
-                                int xx = nx + rx;
-                                int yy = ny + ry;
-                                if (map->inBounds(xx, yy)) {
-                                    Bloc* neighbor = map->getBlock(xx, yy);
-                                    if (neighbor && neighbor->getType() == STONE) {
-                                        stoneCount++;
-                                    }
-                                }
-                            }
-                        }
-                        // Si au moins un STONE autour, on propage un champignon
-                        if (stoneCount > 0) {
-                            map->setBlocInNextFrame(nx, ny, new Mushroom(nx, ny, initialCooldown_));
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // Vérifie s'il y a un bloc vide en dessous et aucune stone autour
+    // Compte les nombre de voisins STONE
     int x = getX();
     int y = getY();
     int stoneNeighbors = 0;
@@ -63,9 +26,73 @@ void Mushroom::update(Map* map) {
         }
     }
 
-    if (stoneNeighbors == 0 && map->inBounds(x, y + 1) && map->getBlock(x, y + 1) == nullptr) {
-        setY(y + 1);
+    // Le champignon s'accroche s'il a un voisin STONE
+    if (stoneNeighbors > 0) {
+        hooked_ = true;
     }
 
-    map->setBlocInNextFrame(getX(), getY(), this);
+    //Comportement non accroché
+    if (hooked_ == false) {
+        // Tombe si pas de voisin STONE et vide en en dessous
+        if (stoneNeighbors == 0 && map->inBounds(x, y + 1) && map->getBlock(x, y + 1) == nullptr) {
+            setY(y + 1);
+        }
+        map->setBlocInNextFrame(getX(), getY(), this);
+    }
+    if (hooked_ == true) {
+        if (stoneNeighbors > 0) {
+            map->setBlocInNextFrame(getX(), getY(), this);
+        }
+        else {
+            map->removeBloc(getX(), getY());
+        }
+    }
+
+    //Propagation
+    if (--spreadCooldown_ <= 0) {
+        spreadCooldown_ = 30;
+
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if (dx == 0 && dy == 0) continue;
+
+                int nx = getX() + dx;
+                int ny = getY() + dy;
+
+                if (map->inBounds(nx, ny)) {
+                    Bloc* adjacent = map->getBlock(nx, ny);
+                    //Si le bloc adjaent est vide
+                    if (adjacent == nullptr) {
+                        // Vérifie s'il y a des blocs STONE autour de la case vide
+                        int stoneCount = 0;
+                        for (int rx = -1; rx <= 1; rx++) {
+                            for (int ry = -1; ry <= 1; ry++) {
+                                if (rx == 0 && ry == 0) continue;
+                                int xx = nx + rx;
+                                int yy = ny + ry;
+                                if (map->inBounds(xx, yy)) {
+                                    Bloc* neighbor = map->getBlock(xx, yy);
+                                    if (neighbor && neighbor->getType() == STONE) {
+                                        stoneCount++;
+                                    }
+                                }
+                            }
+                        }
+                        // Si au moins un STONE autour, on propage un champignon sur cette vase vide
+                        if (stoneCount > 0) {
+                            map->setBlocInNextFrame(nx, ny, new Mushroom(nx, ny, initialCooldown_));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
 }
+
+
+
+
+    
+
